a0beaae6a091468b51df378568f027e8baf5a6e2
RepishnyNikita <nikitarepishny@gmail.com>
2025-04-27 20:14:29 +0300

    fixing paths

--- scripts/events/triggerEvent.js
+++ scripts/events/triggerEvent.js
@@ -1,11 +1,11 @@
 import startEventTimer from "./startEventTimer.js";
-import showEventNotification from "./showEventnotification.js";
+import showEventNotification from "./showEventsNotification.js";
 import { game } from "../variables-game.js";
 
 // Активация события
 export default function triggerEvent(event) {
   game.events.activeEvent = event;
-  showEventNotification(event, event.icon,event.duration);
+  showEventNotification(event, event.icon, event.duration);
   if (event.apply) event.apply();
   if (event.duration) {
     startEventTimer(event.duration);
--- scripts/house/selectHouse.js
+++ scripts/house/selectHouse.js
@@ -1,19 +1,19 @@
 //!ВЫБОР ДОМА
-import { addToLog, clearActions, addAction, clearLog} from "../utils.js";
+import { addToLog, clearActions, addAction, clearLog } from "../utils.js";
 import { game } from "../variables-game.js";
 import generateHouse from "./generateHouse.js";
 import startRobbery from "../startRobbery.js";
 import updateShowTargetItem from "./updateShowTargetItem.js";
 import { containerElements } from "../dom-elements.js";
 import endRobbery from "../endRobbery.js";
-import showEventNotification from "../events/showEventnotification.js";
+import showEventNotification from "../events/showEventsNotification.js";
 import { ICON } from "../assets.js";
 
 const houseIconsSrc = [
-  {house: 'icon/homes/home-1.png'},
-  {house: 'icon/homes/home-2.png'},
-  {house: 'icon/homes/home-3.png'}
-]
+  { house: "icon/homes/home-1.png" },
+  { house: "icon/homes/home-2.png" },
+  { house: "icon/homes/home-3.png" },
+];
 
 export default function selectHouse() {
   clearActions();
@@ -29,32 +29,45 @@ export default function selectHouse() {
     ).length;
     const hasGarage = house.entryPoints.some((point) => point.type === "гараж");
     addToLog(
-      `Дом ${i}: ${doors} ${doors >= 2 ? "двери" : "дверь"}, ${windows} ${windows <= 4 ? "окна" : "окон"}${
-        hasGarage ? ", гараж" : ""
-      }`,
+      `Дом ${i}: ${doors} ${doors >= 2 ? "двери" : "дверь"}, ${windows} ${
+        windows <= 4 ? "окна" : "окон"
+      }${hasGarage ? ", гараж" : ""}`,
       "green",
       ICON.SEARCH
     );
-    
-    addAction(`Дом ${i}`, () => {
-      if(game.energy.currentEnergy <= 0){
-        addToLog(`Недостаточно энергии для входа в дом (нужно 1)`,'red',ICON.X_MARK)
-        return
-      }
-      game.currentHouse = house;
-      game.checkedDoors = [];
-      game.visitedRooms = [];
-      game.targetItemFound = false;
-      containerElements.gamefieldInfo.classList.add('is-visuality')
-      showEventNotification()
-      updateShowTargetItem()
-      startRobbery(i); 
-    },'button-actions-flex reverse', houseIconsSrc[i-1].house);
+
+    addAction(
+      `Дом ${i}`,
+      () => {
+        if (game.energy.currentEnergy <= 0) {
+          addToLog(
+            `Недостаточно энергии для входа в дом (нужно 1)`,
+            "red",
+            ICON.X_MARK
+          );
+          return;
+        }
+        game.currentHouse = house;
+        game.checkedDoors = [];
+        game.visitedRooms = [];
+        game.targetItemFound = false;
+        containerElements.gamefieldInfo.classList.add("is-visuality");
+        showEventNotification();
+        updateShowTargetItem();
+        startRobbery(i);
+      },
+      "button-actions-flex reverse",
+      houseIconsSrc[i - 1].house
+    );
   }
 
-  addAction(null, ()=>{
-    addToLog(`Отмена ограбления!`, "gray",ICON.EXIT_MAN);
-    endRobbery()
-  },'actions-container-cancellation',ICON.X_MARK);
+  addAction(
+    null,
+    () => {
+      addToLog(`Отмена ограбления!`, "gray", ICON.EXIT_MAN);
+      endRobbery();
+    },
+    "actions-container-cancellation",
+    ICON.X_MARK
+  );
 }
-
